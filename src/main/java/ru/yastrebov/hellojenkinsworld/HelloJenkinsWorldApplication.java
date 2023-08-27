bfdbbd294408d3f46c85852f991cbb207257703d
Ястребов Андрей Владимирович (4076463) <ayastrebov@dev.vtb.ru>
2023-08-27 09:22:03 +0300

    Initial commit

---
+++ src/main/java/ru/yastrebov/hellojenkinsworld/HelloJenkinsWorldApplication.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.hellojenkinsworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJenkinsWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJenkinsWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yastrebov/hellojenkinsworld/HelloJenkinsWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.hellojenkinsworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJenkinsWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
